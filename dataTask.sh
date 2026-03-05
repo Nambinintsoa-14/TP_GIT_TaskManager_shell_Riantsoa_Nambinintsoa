9cf71ebf921499b27ac0879d85b991ceafff8442
riantsoa-randria <nrrandriamandimby@student.udm.ac.mu>
2026-03-05 10:53:18 +0400

    feat : ajout fonction afficher_toutes_taches

--- dataTask.sh
+++ dataTask.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 # fonction 1
-
 if [ -f "tasks.txt" ] 
 then
-	echo " the file already exists."
+	echo " Fichier deja existant."
 else
 	touch  "tasks.txt" && echo "The file has been created successfully."
 fi 
+
+
+# fonction 2
+if [ -f "tasks.txt" ]
+then
+	cat "tasks.txt"
+else
+	echo "Base non initialisée!"
+fi
+
+
