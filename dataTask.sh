c07b3a29d3fab3a9928f5fe6d02837c98408dfe9
riantsoa-randria <nrrandriamandimby@student.udm.ac.mu>
2026-03-05 11:16:09 +0400

    feat: ajout fonction sauvegarder_base

--- dataTask.sh
+++ dataTask.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
-# fonction 1
+initialiser_base() {
 if [ -f "tasks.txt" ] 
 then
 	echo " Fichier deja existant."
 else
 	touch  "tasks.txt" && echo "The file has been created successfully."
 fi 
+}
 
 
-# fonction 2
+affiche_toutes_taches() {
 if [ -f "tasks.txt" ]
 then
 	cat "tasks.txt"
 else
 	echo "Base non initialisée!"
 fi
+}
 
+
+
+sauvegader_base() {
+if [ -f "tasks.txt" ]
+then
+	cp tasks.txt  tasks_backup.txt 
+	echo "sauvegarde creee: tasks_backup.txt"
+else
+	echo "Base non initialisée!"
+fi
+}
 
